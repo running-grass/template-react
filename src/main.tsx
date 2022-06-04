537082d7d80e6d766b15266f27b6135782e68659
Leo Liu <467195537@qq.com>
2022-06-04 18:56:42 +0800

    引入vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
-import * as React from 'react'
-import * as Server from 'react-dom/server'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-let Greet = () => <h1>Hello, world!</h1>
-console.log(Server.renderToString(<Greet />))
-console.log(123)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
